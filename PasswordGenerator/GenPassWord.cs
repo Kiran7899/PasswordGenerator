eab6c10f7c524baa1c62aded122682d030b6e6a5
kiran7899@gmail.com <kiran7899@gmail.com>
2022-11-03 19:59:46 +0530

    Finalized code

--- PasswordGenerator/GenPassWord.cs
+++ PasswordGenerator/GenPassWord.cs
@@ -2,6 +2,151 @@
 
 namespace PasswordGenerator
 {
+    #region Method1
+    /*
+    public enum PasswordType
+    {
+        Numeric,
+        AlphaLower,
+        AlphaUpper,
+        AlphaLowerUpper,
+        AlphaNumericLower,
+        AlphaNumericUpper,
+        AlphaNumericLowerUpper 
+    }
+    public class GenPassWord
+    {
+        public string GeneratePassword(int length,PasswordType passwordType)
+        {
+            //if (length < 6)
+            //    return "Password length cannot be less than 6!";
+      
+            Random random = new Random();
+            StringBuilder str = new StringBuilder();
+
+            int upperLength = 0;           
+            int lowerLength = 0;
+            int numericLength = 0;
+            switch (passwordType)
+            {
+                case PasswordType.Numeric:
+                    for(int i = 0; i < length; i++)
+                    {
+                        str.Append(randomInt());
+                    }
+                    break;
+                case PasswordType.AlphaLower:
+                    for(int i=0;i<length;i++)
+                    {                 
+                        str.Append(randomChar(true));
+                    }
+                    break;
+                case PasswordType.AlphaUpper:
+                    for (int i = 0; i < length; i++)
+                    {                     
+                        str.Append(randomChar(false));
+                    }
+                    break;
+                case PasswordType.AlphaLowerUpper:
+                    lowerLength = length / 2;
+                    upperLength = length / 2 + length % 2;
+                    for (int i = 0; i < length / 2; i++)
+                    {
+                        str.Append(randomChar(true));
+                    }
+                    for(int i=0;i<upperLength;i++)
+                    {
+                        str.Append(randomChar(false));
+                    }
+                    break;
+                case PasswordType.AlphaNumericLower:
+                    lowerLength = length / 2;
+                    numericLength = length / 2 + length % 2;
+                    for(int i=0;i<lowerLength;i++)
+                    {
+                        str.Append(randomChar(true));
+                    }
+                    for(int i=0;i<=numericLength;i++)
+                    {
+                        str.Append(randomInt());
+                    }
+                    break;
+
+                case PasswordType.AlphaNumericUpper:
+                    upperLength = length / 2;
+                    numericLength =  length / 2 + length % 2;
+                    for(int i=0;i<upperLength;i++)
+                    {
+                        str.Append(randomChar(false));
+                    }
+                    for(int i=0;i<numericLength;i++)
+                    {
+                        str.Append(randomInt());
+                    }
+                    break;
+                case PasswordType.AlphaNumericLowerUpper:
+                    numericLength = length / 3;
+                    lowerLength = length / 3;
+                    upperLength = length % 3 + length / 3;
+                    for(int i = 0; i < numericLength; i++)
+                    {
+                        str.Append(randomInt());
+                    }
+                    for (int i = 0; i < lowerLength; i++)
+                    {
+                        str.Append(randomChar(true));
+                    }
+                    for(int i=0;i<upperLength;i++)
+                    {
+                        str.Append(randomChar(false));
+                    }
+                    break;
+
+            }
+
+            return ShufflePassword(str.ToString());
+        }
+        private string ShufflePassword(string password)
+        {
+            int length = password.Length;
+            HashSet<int> index = new HashSet<int>();
+            StringBuilder str = new StringBuilder();    
+            Random random = new Random();
+            while(index.Count != length)
+            {
+                int val = random.Next(0,length-1);
+                if (!index.Contains(val))
+                {
+                    index.Add(val);
+                    str.Append(password[val]);
+                }
+            }
+            return str.ToString();
+        }
+        private int randomInt()
+        {
+            Random r = new Random();
+            return r.Next(0, 9);
+        }
+        private char randomChar(bool isLower)
+        {
+            Random random = new Random();   
+            int num = random.Next(0, 25);
+            char ch = '.';
+            if(isLower)
+            {
+                ch = (char)(97 + num);
+            }
+            else
+            {
+                ch = (char)(65 + num);
+            }
+            return ch;
+        }
+    }
+    */
+    #endregion
+
     public enum PasswordType
     {
         Numeric,
---
+++ PasswordGenerator/ReadMe.txt
@@ -0,0 +1,4 @@
+﻿Name : Kirankumar Balasubramanian
+Time Spent : 1 hour
+Comments : Tried different approach which is commented under Method1 region. 
+		   Switched to new approach as that is not generating the random string properly
