8f667e06067874ff7cd1b9d3ecc62de0ccad06f3
kiran7899@gmail.com <kiran7899@gmail.com>
2022-11-03 19:51:26 +0530

    Add project files.

---
+++ ClientPasswordGen/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PasswordGenerator;
+
+Console.WriteLine("Hello, World!");
+GenPassword password = new GenPassword();
+Console.WriteLine(password.GeneratePassword(41, PasswordType.Numeric));
+Console.WriteLine(password.GeneratePassword(33, PasswordType.AlphaLower));
+Console.WriteLine(password.GeneratePassword(41, PasswordType.AlphaUpper));
+Console.WriteLine(password.GeneratePassword(32, PasswordType.AlphaLowerUpper));
+Console.WriteLine(password.GeneratePassword(19, PasswordType.AlphaNumericUpper));
+Console.WriteLine(password.GeneratePassword(18, PasswordType.AlphaNumericLower));
+Console.WriteLine(password.GeneratePassword(16, PasswordType.AlphaNumericLowerUpper));
+Console.WriteLine(password.GeneratePassword(1, PasswordType.AlphaNumericLowerUpper));
---
+++ PasswordGenerator/GenPassWord.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+
+namespace PasswordGenerator
+{
+    public enum PasswordType
+    {
+        Numeric,
+        AlphaLower,
+        AlphaUpper,
+        AlphaLowerUpper,
+        AlphaNumericLower,
+        AlphaNumericUpper,
+        AlphaNumericLowerUpper
+    }
+    public class GenPassword
+    {
+        public string GeneratePassword(int length,PasswordType passwordType)
+        {
+            if (length < 6)
+                throw new Exception("Password length should be atleast 6");
+            string allowedChars = "";
+            string password = "";
+            switch (passwordType)
+            {
+                case PasswordType.Numeric:
+                    allowedChars = "0123456789";
+                    password = GenerateRandomPassword(length, allowedChars);
+                    break;
+                case PasswordType.AlphaLower:
+                    allowedChars = "abcdefghijklmnopqrstuvwxyz";
+                    password = GenerateRandomPassword(length, allowedChars);
+                    break;
+                case PasswordType.AlphaUpper:
+                    allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                    password = GenerateRandomPassword(length, allowedChars);
+                    break;
+                case PasswordType.AlphaLowerUpper:
+                    allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                    password = GenerateRandomPassword(length, allowedChars);
+                    break;
+                case PasswordType.AlphaNumericLower:
+                    allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789";
+                    password = GenerateRandomPassword(length, allowedChars);
+                    break;
+                case PasswordType.AlphaNumericUpper:
+                    allowedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                    password = GenerateRandomPassword(length, allowedChars);
+                    break;
+                case PasswordType.AlphaNumericLowerUpper:
+                    allowedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+                    password = GenerateRandomPassword(length, allowedChars);
+                    break;
+                default:
+                    break;
+            }
+            return password;
+        }
+
+        private string GenerateRandomPassword(int length, string allowedChars)
+        {
+            Random random = new Random();
+            StringBuilder sb = new StringBuilder(); 
+            for(int i = 0; i < length; i++)
+            {
+                int index = random.Next(0, allowedChars.Length-1);
+                sb.Append(allowedChars[index]);
+            }
+            return sb.ToString();
+        }
+    }
+}
